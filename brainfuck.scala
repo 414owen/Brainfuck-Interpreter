d916ad1c6da624d8b6b29902e34e10312c96a6b6
Owen Shepherd <shephero@tcd.ie>
2016-05-16 16:13:25 +0100

    Added another entry point (instead of main)

--- brainfuck.scala
+++ brainfuck.scala
@@ -1,30 +1,45 @@
+import scala.annotation.tailrec
+
 object Brainfuck {
+    def headOrZero(list: List[Byte]): Byte = list match {
+        case x :: xs => x
+        case Nil => 0
+    }
+
+    def tailOrSame(list: List[Byte]): List[Byte] = list match {
+        case x :: xs => xs
+        case Nil => Nil
+    }
+    
+    @tailrec def mutateToEnd(mach: Machine): Option[Machine] = 
+        mach.nextState match {
+            case Some(m) => mutateToEnd(m)
+            case None => None
+    }
+
+    def main(args: Array[String]) {
+        run(scala.io.Source.fromFile(args(0))
+            .mkString.filter("[]<>+-.,".contains(_)))
+    }
+
+    def run(program: String): Unit = {
+        mutateToEnd(Machine(List(), List(), 0.toByte, program, 0))
+    }
+
     case class Machine (left: List[Byte], right: List[Byte],
                    current: Byte, program: String, 
                    instruction: Int) {
 
         def memLeft(): Machine = {
-            val newCurrent: Byte = left match {
-                case x :: rest => x
-                case nil => 0
-            }
-            val newLeft: List[Byte] = left match {
-                case x :: rest => rest
-                case nil => List(0)
-            }
+            val newCurrent: Byte = headOrZero(left)
+            val newLeft: List[Byte] = tailOrSame(left)
             Machine(newLeft, current::right, 
                 newCurrent, program, instruction)
         }
 
         def memRight(): Machine = {
-            val newCurrent: Byte = right match {
-                case x :: rest => x
-                case nil => 0
-            }
-            val newRight: List[Byte] = right match {
-                case x :: rest => rest
-                case nil => List(0)
-            }
+            val newCurrent: Byte = headOrZero(right)
+            val newRight: List[Byte] = tailOrSame(right)
             Machine(current::left, newRight,
                 newCurrent, program, instruction)
         }
@@ -33,17 +48,16 @@ object Brainfuck {
 
         def editMem(f: Byte => Byte): Machine = copy(current = f(current))
 
+        @tailrec 
         final def matchBracket(brac: Char, depth: Int = 0): Machine = {
             val otherBrac = if (brac == '[') ']' else '['
             val inc = if (brac == '[') 1 else -1
-            val machNext = Machine(
-                left, right,
-                current, program,
-                instruction + inc)
+            val machNext = Machine(left, right, 
+                current, program, instruction + inc)
             program(machNext.instruction) match {
                 case `brac` => machNext.matchBracket(brac, depth - inc)
                 case `otherBrac` => if (depth == 0) machNext
-                        else machNext.matchBracket(brac, depth + inc)
+                        else machNext .matchBracket(brac, depth + inc)
                 case _ => machNext.matchBracket(brac, depth)
             }
         }
@@ -77,17 +91,5 @@ object Brainfuck {
                     Some(machNext.codeRight)
             else None
         }
-    }
-
-    def main(args: Array[String]) {
-        def mutateToEnd(mach: Machine): Option[Machine] = 
-            mach.nextState match {
-                case Some(m) => mutateToEnd(m)
-                case None => None
-        }
-
-        val program = scala.io.Source.fromFile(args(0))
-            .mkString.filter("[]<>+-.,".contains(_))
-        mutateToEnd(Machine(List(), List(), 0.toByte, program, 0))
     }
 }
